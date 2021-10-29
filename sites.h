46878df843d8dc04e46513703023e238e70e535b
Yago Carro <ycarro@c2r3s1.42madrid.com>
2021-10-29 16:07:39 +0200

    v0.4

--- sites.h
+++ sites.h
@@ -96,3 +96,86 @@ const char Success[] PROGMEM = R"=====(
 </body>
 </html>
 )=====";
+
+const char Menu[] PROGMEM = R"=====(
+<html>
+<head>
+    <title>DeBrujin</title>
+    <meta charset="utf-8">
+    <style>
+        body {
+            background-color: #000000;
+            font-family: "Century Gothic", Arial;
+            color: white;
+            margin: 20px;
+        }
+       .button-30 {
+          align-items: center;
+          appearance: none;
+          background-color: #FCFCFD;
+          border-radius: 40px;
+          border-width: 0;
+          box-shadow: rgba(45, 35, 66, 0.4) 0 2px 4px,rgba(45, 35, 66, 0.3) 0 7px 13px -3px,#D6D6E7 0 -3px 0 inset;
+          box-sizing: border-box;
+          color: #36395A;
+          cursor: pointer;
+          display: inline-flex;
+          font-family: "JetBrains Mono",monospace;
+          width: 800px;
+          height: 200px;
+          justify-content: center;
+          line-height: 1;
+          list-style: none;
+          overflow: hidden;
+          padding-left: 16px;
+          padding-right: 16px;
+          position: relative;
+          text-align: left;
+          text-decoration: none;
+          transition: box-shadow .15s,transform .15s;
+          user-select: none;
+          -webkit-user-select: none;
+          touch-action: manipulation;
+          white-space: nowrap;
+          will-change: box-shadow,transform;
+          font-size: 80px;
+        }
+        .button-30:focus {
+          box-shadow: #D6D6E7 0 0 0 1.5px inset, rgba(45, 35, 66, 0.4) 0 2px 4px, rgba(45, 35, 66, 0.3) 0 7px 13px -3px, #D6D6E7 0 -3px 0 inset;
+        }
+        .button-30:hover {
+          box-shadow: rgba(45, 35, 66, 0.4) 0 4px 8px, rgba(45, 35, 66, 0.3) 0 7px 13px -3px, #D6D6E7 0 -3px 0 inset;
+          transform: translateY(-2px);
+        }
+        .button-30:active {
+          box-shadow: #D6D6E7 0 3px 7px inset;
+          transform: translateY(2px);
+        }
+        .title {
+            font-size: 100px;
+            font-family: Monaco, Monospace;
+        }
+        .sub {
+            font-size: 70px;
+            font-family: "Lucida Handwriting", Cursive;
+        }
+        }
+    </style>
+</head>
+<body>
+<center>
+<br>
+<h1 class="title">Nix</h1>
+<p class="sub">The RF DeBrujin sequence transmitter for EvilCrowRF.</p>
+<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+<div id="header">
+    <div id="menu">
+        <ul>
+            <a class="button-30" href="/debrujin">DeBrujin Attack</a>
+        </ul>
+    </div>
+</div>
+</center>
+</body>
+</html>
+)=====";
